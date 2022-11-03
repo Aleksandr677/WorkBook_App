adfaf88189c1b901d459ea92378fdfbd74f9dc5e
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-11-03 18:02:28 +0300

    feat: add BackgroundImageView, NewTaskItemView, BlankView

---
+++ Devote_App/Devote_App/View/BackgroundImageView.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundImageView.swift
+//  Devote_App
+//
+//  Created by Христиченко Александр on 2022-11-03.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("rocket")
+            .antialiased(true)
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea(.all)
+    }
+}
+
+struct BackgroundImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundImageView()
+    }
+}
